91d966e4fd4cf94732f8dfd46200a60ff067f5d6
yossi <yossix07@gmail.com>
2021-10-23 18:22:34 +0300

    add files

--- client.py
+++ client.py
@@ -2,13 +2,21 @@ import socket
 import sys
 
 if __name__ == '__main__':
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     file = open(sys.argv(2))
     if(len(file.read())) > 100:
+        n = 100
+        chunks = [str[i:i + n] for i in range(0, len(str), n)]
+        for chunk in chunks:
+            s.sendto(chunk, (sys.argv(0), sys.argv(1)))
+            data, addr = s.recvfrom(1024)
 
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.sendto(file.read(), (sys.argv(0) , sys.argv(1)))
-    data, addr = s.recvfrom(1024)
+
+    else:
+     s.sendto(file.read(), (sys.argv(0) , sys.argv(1)))
+     data, addr = s.recvfrom(1024)
+
     print(str(data), addr)
     s.close()
 
