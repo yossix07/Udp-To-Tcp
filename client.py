9be79e1cfc2306dc66cb8707cccd5fa998886724
naor <naor9985@gmail.com>
2021-10-23 18:22:33 +0200

    update

--- client.py
+++ client.py
@@ -8,11 +8,10 @@ if __name__ == '__main__':
         n = 100
         chunks = [str[i:i + n] for i in range(0, len(str), n)]
         for chunk in chunks:
-            s.sendto(bytes(chunk), (sys.argv[0], sys.argv[1]))
+            s.sendto(chunk, (sys.argv[0], sys.argv[1]))
             data, addr = s.recvfrom(1024)
-
     else:
-        s.sendto(bytes(file.read()), (sys.argv[0], sys.argv[1]))
+        s.sendto(bytes(file.read(), 'UTF-8'), (sys.argv[1], int(sys.argv[2])))
         data, addr = s.recvfrom(1024)
 
     print(str(data), addr)
