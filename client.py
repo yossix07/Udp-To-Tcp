62c10171faee0f002fed4188c19a80b246a43012
naor <naor9985@gmail.com>
2021-10-24 02:33:56 +0200

    define main

--- client.py
+++ client.py
@@ -1,20 +1,29 @@
 import socket
 import sys
 
-if __name__ == '__main__':
+
+def main(ip_address, port_number, patch_file):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    file = open(sys.argv[3], "r")
+    file = open(patch_file, "r")
     file_string = file.read().replace('\n',' ')
     size = 100
     if len(file_string) >= size:
-        chunks = [file_string[i:i+size] for i in range(0,len(file_string),size)]
+        chunks = [file_string[i:i+size] for i in range(0, len(file_string), size)]
         for chunk in chunks:
-            s.sendto(bytes(chunk, 'utf-8'), (sys.argv[1], int(sys.argv[2])))
+            s.sendto(bytes(chunk, 'utf-8'), (ip_address, int(port_number)))
             data, addr = s.recvfrom(1024)
+            #send(data, addr)
     else:
-        s.sendto(bytes(file_string, 'utf-8'), (sys.argv[1], int(sys.argv[2])))
+        s.sendto(bytes(file_string, 'utf-8'), (ip_address, int(port_number)))
         data, addr = s.recvfrom(1024)
-
+        #send(data, addr)
     print(str(data), addr)
     s.close()
     file.close()
+
+
+if __name__ == '__main__':
+    ip_address = sys.argv[1]
+    port_number = sys.argv[2]
+    patch_file = sys.argv[3]
+    main(ip_address, port_number, patch_file)
