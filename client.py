1029000c99217b863e6fd64c59ae12c68e7e7967
yossix07 <79411610+yossix07@users.noreply.github.com>
2021-10-28 00:19:16 +0300

    add constants and documentary

--- client.py
+++ client.py
@@ -2,29 +2,76 @@ import socket
 import sys
 
 
-def main(ip_address, port_number, patch_file):
+# constants
+class CONST:
+    @staticmethod
+    def DATA_SIZE():
+        return 98
+
+    @staticmethod
+    def ID_SIZE():
+        return 2
+
+    @staticmethod
+    def TIME_DELAY():
+        return 0.5
+
+    @staticmethod
+    def DATA_SIZE_LIMIT():
+        return 1024
+
+    @staticmethod
+    def ARG_ONE():
+        return 1
+
+    @staticmethod
+    def ARG_TWO():
+        return 1
+
+    @staticmethod
+    def ARG_THREE():
+        return 1
+
+    @staticmethod
+    def FIRST_ID():
+        return 0
+
+
+# read the received file's text and
+# send it to application with the revived port, on the server with the received ip address
+def client(ip_address, port_number, patch_file):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
         with open(patch_file, "rb") as file:
-            header_id = 0
-            while True:
-                chunk = file.read(98)
-                if chunk == b'':
-                    break
-                id_chunk = header_id.to_bytes(2, 'little') + chunk
+
+            # read first 98 bytes from the file
+            chunk = file.read(CONST.DATA_SIZE())
+            header_id = CONST.FIRST_ID()
+
+            # send all the text bytes from the file to the relevant application on the relevant server
+            while chunk != b'':
+
+                # stores the package number and it's text data
+                id_chunk = header_id.to_bytes(CONST.ID_SIZE(), 'little') + chunk
+
+                # sends the package to the server until the server approval of getting it
                 while True:
                     s.sendto(id_chunk, (ip_address, int(port_number)))
-                    s.settimeout(0.5)
+                    s.settimeout(CONST.TIME_DELAY())
                     try:
-                        data, addr = s.recvfrom(1024)
+                        data, address = s.recvfrom(CONST.DATA_SIZE_LIMIT())
+
+                        # when server approve getting current package, move to the next one and increase the id count
                         if data == chunk:
-                            print(str(data), addr)
                             header_id += 1
                             break
                         else:
                             continue
                     except socket.timeout:
+
+                        # in case of failed delivery, try again
                         continue
+                chunk = file.read(CONST.DATA_SIZE())
     except ValueError:
         print("Error - wrong patch")
         sys.exit(1)
@@ -33,12 +80,14 @@ def main(ip_address, port_number, patch_file):
         file.close()
 
 
+# runs client's program - user enters ip address of a server, an application's port number and a patch to text file
+# and send it to the relevant application on the relevant server
 if __name__ == '__main__':
     try:
-        ip_address = sys.argv[1]
-        port_number = sys.argv[2]
-        patch_file = sys.argv[3]
-        main(ip_address, port_number, patch_file)
+        ip_address = sys.argv[CONST.ARG_ONE()]
+        port_number = sys.argv[CONST.ARG_TWO()]
+        patch_file = sys.argv[CONST.ARG_THREE()]
+        client(ip_address, port_number, patch_file)
     except ValueError:
         print("Error - wrong arguments")
         sys.exit(1)
